c87badf2edd3c6d397ee248fdaa603038ee0ac15
haodong191 <hdong215@gmail.com>
2021-09-05 17:45:46 -0400

    Work in progress

---
+++ Missions_to_Mars/.ipynb_checkpoints/scrape_mars-checkpoint.py
@@ -0,0 +1,63 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+    executable_path = {"executable_path": "chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
+    
+    browser.visit('https://redplanetscience.com/')
+    time.sleep(5)
+    news_html = browser.html
+    soup = BeautifulSoup(news_html, 'html.parser')
+    news_title = soup.find_all('div', class_='content_title')[0].text
+    news_p = soup.find_all('div', class_='article_teaser_body')[0].text
+
+
+    browser.visit('https://spaceimages-mars.com/')
+    time.sleep(5)
+    baseurl = 'https://spaceimages-mars.com/'
+    browser.links.find_by_partial_text('FULL IMAGE').click()
+    image_html = browser.html
+    image_soup = BeautifulSoup(image_html, 'html.parser')
+    image_results = image_soup.find('img', class_='fancybox-image')['src']
+    featured_image_url = baseurl + image_results
+
+
+    browser.visit('https://galaxyfacts-mars.com')
+    facts=pd.read_html('https://galaxyfacts-mars.com')
+    mars_facts= facts[1]
+    mars_fact_table = mars_facts.to_html()
+
+
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    hemispheres_html = browser.html
+    soup = BeautifulSoup(hemispheres_html, 'html.parser')
+    hemisphere_image_urls = []
+    hemispheres = soup.find_all('div', class_='item')
+
+    for hemisphere in hemispheres:
+        hemisphere_header = hemisphere.h3.text.strip()
+        hemisphere_url = hemisphere.a['href']
+        browser.visit(url + hemisphere_url)
+        hemisphere_html = browser.html
+        hemisphere_soup = BeautifulSoup(hemisphere_html, 'html.parser')
+        hemisphere_finder = hemisphere_soup.find('div', id='wide-image')
+        hemisphere_finder2 = hemisphere_finder.find('a', target='_blank')['href']
+        hemisphere_image_url = url +  hemisphere_finder2
+        hemisphere_image_urls.append({"title":hemisphere_header, "img_url":hemisphere_image_url})
+        browser.links.find_by_partial_text('Back').click()
+        time.sleep(1)
+
+    mars_html = {
+    "news_title":news_title,
+    "news_p": news_p,
+    "featured_img_url": featured_image_url,
+    "facts":mars_fact_table,
+    "hemispheres":hemisphere_image_urls
+    }
+
+    return mars_html
